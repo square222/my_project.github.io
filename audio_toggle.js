6ff60a07e03399a420e631827052cee45cba5a73
square222 <98166571+square222@users.noreply.github.com>
2024-10-23 17:38:19 +0700

    Add files via upload

---
+++ audio_toggle.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button1 = document.querySelector('.block_button1'); 
+  const button2 = document.querySelector('.block_button2');
+
+  if (button1 && button2) { 
+      const audio = new Audio('joy2.wav'); 
+
+      button1.addEventListener('click', () => { 
+          if (audio.paused) {
+              audio.play();
+          } else {
+              audio.pause();
+          }
+      });
+
+      button2.addEventListener('click', () => { 
+          window.location.href = 'https://disk.yandex.ru/client/disk/test/1941%20Социалистическое%20льноводство/PDF', '_blank'; 
+      });
+  } else {
+      console.error('Одна или обе кнопки не найдены');
+  }
+});
