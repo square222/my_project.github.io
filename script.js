a86220e3d569fbd1279ca7dc03b674d9a0f3f713
square222 <98166571+square222@users.noreply.github.com>
2024-10-25 16:29:01 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,99 +3,95 @@ const paginationContainer = document.getElementById('pagination');
 const prevButton = document.getElementById('prev-button');
 const nextButton = document.getElementById('next-button');
 const buttonsPerPage = 10;
-const totalButtons = 88;
 let currentPage = 1;
 
-// Массив с ссылками (замените на свои ссылки)
+// Массив с ссылками
 const links = [
-    'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/002/',
-    'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/003/',
-    'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/004/',
-    'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/005/',
-    'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/006/',
-    'https://ссылка6.com',
-    'https://ссылка7.com',
-    'https://ссылка8.com',
-    'https://ссылка9.com',
-    'https://ссылка10.com',
-    'https://ссылка11.com',
-    'https://ссылка12.com',
-    'https://ссылка13.com',
-    'https://ссылка14.com',
-    'https://ссылка15.com',
-    'https://ссылка16.com',
-    'https://ссылка17.com',
-    'https://ссылка18.com',
-    'https://ссылка19.com',
-    'https://ссылка20.com',
-    'https://ссылка21.com',
-    'https://ссылка22.com',
-    'https://ссылка23.com',
-    'https://ссылка24.com',
-    'https://ссылка25.com',
-    'https://ссылка26.com',
-    'https://ссылка27.com',
-    'https://ссылка28.com',
-    'https://ссылка29.com',
-    'https://ссылка30.com',
-    'https://ссылка31.com',
-    'https://ссылка32.com',
-    'https://ссылка33.com',
-    'https://ссылка34.com',
-    'https://ссылка35.com',
-    'https://ссылка36.com',
-    'https://ссылка37.com',
-    'https://ссылка38.com',
-    'https://ссылка39.com',
-    'https://ссылка40.com',
-    'https://ссылка41.com',
-    'https://ссылка42.com',
-    'https://ссылка43.com',
-    'https://ссылка44.com',
-    'https://ссылка45.com',
-    'https://ссылка46.com',
-    'https://ссылка47.com',
-    'https://ссылка48.com',
-    'https://ссылка49.com',
-    'https://ссылка50.com',
-    'https://ссылка51.com',
-    'https://ссылка52.com',
-    'https://ссылка53.com',
-    'https://ссылка54.com',
-    'https://ссылка55.com',
-    'https://ссылка56.com',
-    'https://ссылка57.com',
-    'https://ссылка58.com',
-    'https://ссылка59.com',
-    'https://ссылка60.com',
-    'https://ссылка61.com',
-    'https://ссылка62.com',
-    'https://ссылка63.com',
-    'https://ссылка64.com',
-    'https://ссылка65.com',
-    'https://ссылка66.com',
-    'https://ссылка67.com',
-    'https://ссылка68.com',
-    'https://ссылка69.com',
-    'https://ссылка70.com',
-    'https://ссылка71.com',
-    'https://ссылка72.com',
-    'https://ссылка73.com',
-    'https://ссылка74.com',
-    'https://ссылка75.com',
-    'https://ссылка76.com',
-    'https://ссылка77.com',
-    'https://ссылка78.com',
-    'https://ссылка79.com',
-    'https://ссылка80.com',
-    'https://ссылка81.com',
-    'https://ссылка82.com',
-    'https://ссылка83.com',
-    'https://ссылка84.com',
-    'https://ссылка85.com',
-    'https://ссылка86.com',
-    'https://ссылка87.com',
-    'https://ссылка88.com',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/002',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/003',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/004',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/005',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/006',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/007',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/008',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/009',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/010',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/011',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/012',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/013',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/014',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/015',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/016',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/018',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/019',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/020',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/021',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/024',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/025',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/026',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/027',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/028',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/029',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/030',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/031',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/032',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/033',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/034',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/035',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/036',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/037',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/038',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/039',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/040',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/041',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/042',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/043',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/045',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/047',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/048',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/049',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/051',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/052',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/053',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/054',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/055',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/056',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/057',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/058',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/059',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/060',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/061',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/062',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/063',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/065',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/066',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/067',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/068',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/069',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/070',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/071',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/072',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/073',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/074',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/076',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/077',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/078',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/079',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/081',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/083',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/084',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/085',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/086',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/087',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/088',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/089',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/091',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/092',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/093',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/094',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/095',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/096',
+  'https://poisk.ngonb.ru/flip/nso/maslyanino/soc_lnovodstvo/1941/098'
 ];
 
 function createButton(link, title) {
@@ -103,35 +99,49 @@ function createButton(link, title) {
     button.classList.add('button');
     button.textContent = title;
     button.addEventListener('click', () => {
-        window.location.href = link;
+      window.location.href = link;
     });
     return button;
-}
-
-function renderButtons() {
+  }
+  
+  function renderButtons() {
     buttonContainer.innerHTML = ''; // Очищаем предыдущие кнопки
-
+  
+    let buttonCount = 0; // Счетчик для подсчета реального количества кнопок
     const startIndex = (currentPage - 1) * buttonsPerPage;
-    const endIndex = Math.min(startIndex + buttonsPerPage, totalButtons);
-
-    for (let i = startIndex; i < endIndex; i++) {
-        buttonContainer.appendChild(createButton(links[i], `Издание №${i + 1}`));
+  
+    // Используем цикл while, чтобы проверить все номера кнопок
+    let i = 0;
+    while (buttonCount < startIndex + buttonsPerPage) {
+      if (i >= links.length) {
+        break; // Прерываем цикл, если достигли конца массива links
+      }
+  
+      const link = links[i];
+      // Извлекаем номер издания из ссылки
+      const issueNumber = link.substring(link.lastIndexOf('/') + 1); 
+      // Форматируем номер с добавлением ведущего нуля, если нужно
+      const formattedIssueNumber = issueNumber.padStart(2, '0');
+      buttonContainer.appendChild(createButton(link, `Издание №${formattedIssueNumber}`)); 
+      
+      buttonCount++;
+      i++;
     }
-}
-
-function updatePaginationButtons() {
+  }
+  
+  function updatePaginationButtons() {
     prevButton.disabled = currentPage === 1;
-    nextButton.disabled = currentPage === Math.ceil(totalButtons / buttonsPerPage);
-}
-
-function handlePageChange(newPage) {
+    nextButton.disabled = currentPage * buttonsPerPage >= links.length; 
+  }
+  
+  function handlePageChange(newPage) {
     currentPage = newPage;
-    renderButtons();
+    renderButtons(); // Перерисовываем все кнопки
     updatePaginationButtons();
-}
-
-prevButton.addEventListener('click', () => handlePageChange(currentPage - 1));
-nextButton.addEventListener('click', () => handlePageChange(currentPage + 1));
-
-renderButtons();
-updatePaginationButtons();
+  }
+  
+  prevButton.addEventListener('click', () => handlePageChange(currentPage - 1));
+  nextButton.addEventListener('click', () => handlePageChange(currentPage + 1));
+  
+  renderButtons();
+  updatePaginationButtons();
